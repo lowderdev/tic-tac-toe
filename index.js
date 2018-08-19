e1c5b4fc8ca86f23f7cd9530ce069ed95d0f9c09
Logan Lowder <logan.lowder@metova.com>
2018-08-18 19:24:35 -0500

    Remove typing message if player clears message input

--- index.js
+++ index.js
@@ -2,19 +2,47 @@ const express = require('express');
 const socket = require('socket.io');
 
 const app = express();
-const server = app.listen(4000, () => { console.log('listening on port 4000,') });
+const server = app.listen(4000, () => {
+  console.log();
+  console.log('=======================');
+  console.log('listening on port 4000,');
+});
+
 app.use(express.static('public'));
 
 const io = socket(server);
+
+let playerNum = 1;
+let players = {};
+
 io.on('connection', (socket) => {
-  console.log('made socket connection', socket.id);
+  console.log(`connecting player ${playerNum}`, socket.id);
+  players[socket.id] = playerNum
+  socket.emit('connected', { id: socket.id, playerNum: playerNum });
+  socket.broadcast.emit('chat', { handle: 'Server', message: `Player${playerNum} connected.` });
+  playerNum++;
 
   socket.on('chat', (data) => {
     console.log(data);
-    io.sockets.emit('chat', data);
+    io.emit(
+      'chat',
+      { handle: `Player${players[data.id]}`, message: data.message }
+    );
   });
 
   socket.on('typing', (data) => {
+    console.log(data);
     socket.broadcast.emit('typing', data);
+  });
+
+  socket.on('move', (data) => {
+    console.log('move');
+    console.log(`Making move on ${data}`);
+    io.emit('move', data);
+  });
+
+  socket.on('disconnect', () => {
+    const departingPlayerNum = players[socket.id];
+    io.emit('chat', { handle: 'Server', message: `Player${departingPlayerNum} disconnected.` });
   });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,29 +1,52 @@
 const socket = io.connect('http://localhost:4000');
 
+const title = document.getElementById('title');
 const output = document.getElementById('output');
 const feedback = document.getElementById('feedback');
-const handle = document.getElementById('handle');
 const message = document.getElementById('message');
 const button = document.getElementById('send');
 
+let id = null;
+let playerNum = null;
+let currentlyTyping = false;
+
 button.addEventListener('click', () => {
   socket.emit('chat', {
-    message: message.value,
-    handle: handle.value
+    id: id,
+    message: message.value
   });
   message.value = '';
 });
 
-message.addEventListener('keypress', () => {
-  socket.emit('typing', handle.value);
-})
+message.addEventListener('input', (event) => {
+  if (currentlyTyping === !!message.value) return;
+  currentlyTyping = !!message.value;
+  socket.emit('typing', { playerNum: playerNum, typing: currentlyTyping });
+});
 
 // Listen for events
+socket.on('connected', (data) => {
+  id = data.id;
+  playerNum = data.playerNum;
+  console.log(`id: ${id}, playerNum: ${playerNum}`)
+  title.innerHTML = `Player${playerNum}`;
+});
+
 socket.on('chat', (data) => {
   feedback.innerHTML = '';
   output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
 });
 
 socket.on('typing', (data) => {
-  feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+  if (data.typing) {
+    feedback.innerHTML = `<p><em>Player${data.playerNum} is typing a message...</em></p>`;
+  } else {
+    feedback.innerHTML = '';
+  }
+});
+
+// tic-tac-toe
+socket.on('move', (data) => {
+  console.log(`Making move on ${data}`);
+  document.getElementById(data).innerHTML = 'X';
 });
